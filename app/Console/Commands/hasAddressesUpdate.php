543825d395ccdddea789a701723ad26b14d6b22e
kareem hossam <kareem_hossam10@yahoo.com>
2021-09-22 01:34:39 +0200

    Task3 fixes

---
+++ app/Console/Commands/hasAddressesUpdate.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Address;
+use App\Models\Area;
+
+class hasAddressesUpdate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:hasAddressesUpdate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'updates hasAddresses col in Areas table';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+       $addressesAreasIds=Address::select('areaId')->distinct()->get();
+
+       foreach($addressesAreasIds as $addressesAreasId){
+
+        Area::where('id', $addressesAreasId->areaId)
+        ->update([
+            'hasAddresses'=>true
+        ]);
+       }
+    }
+}
